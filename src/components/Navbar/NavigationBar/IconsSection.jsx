3542cf17c0ccd3004b8c717f4a30ec7138c0e3f7
Nicole Tevez <nicole.tevez@fizzmod.com>
2021-09-08 19:59:06 -0300

    media queries

--- src/components/Navbar/NavigationBar/IconsSection.jsx
+++ src/components/Navbar/NavigationBar/IconsSection.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import Icon from './Icon'
+import React from "react";
+import Icon from "./Icon";
 
 const IconsSection = () => {
-    return (
-        <div className="icon-section__container">
-            <Icon><i className="icon fas fa-search"></i></Icon>
-            <Icon><i className="icon fas fa-user"></i></Icon>
-            <Icon><i className="icon fas fa-shopping-cart"></i></Icon>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="icon-section__container">
+        <Icon>
+          <i className="icon fas fa-search"></i>
+        </Icon>
+        <Icon>
+          <i className="icon fas fa-user"></i>
+        </Icon>
+        <Icon>
+          <i className="icon fas fa-shopping-cart"></i>
+        </Icon>
+      </div>
+    </>
+  );
+};
 
-export default IconsSection
+export default IconsSection;
