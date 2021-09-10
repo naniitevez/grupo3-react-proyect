21452303cb3bf2563cdfe73c7e5fad879103d091
Nicole Tevez <nicole.tevez@fizzmod.com>
2021-09-10 16:22:13 -0300

    dynamic notification number

--- src/components/Navbar/CartNotification/CartNotification.jsx
+++ src/components/Navbar/CartNotification/CartNotification.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const CartNotification = () => {
+  let numberOfNotifications = 24
   return (
     <span id="cart-notification__container">
-        23
+        {numberOfNotifications}
     </span>
   );
 };
